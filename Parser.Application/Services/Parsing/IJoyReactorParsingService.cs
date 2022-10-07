0fe175cc147f5cee7f3ef5547fcbce236ca12583
kostyabek <kostyabek@gmail.com>
2022-10-07 10:37:08 +0300

    Added parsing service

---
+++ Parser.Application/Services/Parsing/IJoyReactorParsingService.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+
+namespace Parser.Application.Services.Parsing;
+
+/// <summary>
+/// Joyreactor parsing service.
+/// </summary>
+public interface IJoyReactorParsingService
+{
+    /// <summary>
+    /// Parses joyreactor page.
+    /// </summary>
+    /// <param name="postNode">A node with a post.</param>
+    /// <returns>Collection of extracted URLs to posts.</returns>
+    IEnumerable<string> ParsePost(HtmlNode postNode);
+
+    /// <summary>
+    /// Parses joyreactor post.
+    /// </summary>
+    /// <param name="html">Raw HTML.</param>
+    /// <returns>Collection of extracted nodes of posts and a url to the next page.</returns>
+    (IEnumerable<HtmlNode>, string) ParsePage(string html);
+}
---
+++ Parser.Infrastructure/Services/Parsing/JoyReactorParsingService.cs
@@ -0,0 +1,47 @@
+﻿using HtmlAgilityPack;
+using Parser.Application.Services.Parsing;
+
+namespace Parser.Infrastructure.Services.Parsing;
+
+/// <inheritdoc cref="IJoyReactorParsingService"/>
+public class JoyReactorParsingService : IJoyReactorParsingService
+{
+    /// <inheritdoc/>
+    public IEnumerable<string> ParsePost(HtmlNode postNode)
+    {
+        var imagesUrls = postNode
+            .Descendants("div")
+            .Where(e => e.HasClass("post_content"))
+            .SelectMany(e => e.Descendants("div"))
+            .Where(e => e.HasClass("image"))
+            .SelectMany(e => e.Descendants("img"))
+            .Select(e => e.GetAttributeValue("src", "*"))
+            .ToList();
+
+        return imagesUrls;
+    }
+
+    /// <inheritdoc/>
+    public (IEnumerable<HtmlNode>, string) ParsePage(string html)
+    {
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(html);
+
+        var postNodes = htmlDoc.DocumentNode
+            .Descendants("div")
+            .Where(e => e.HasClass("postContainer"))
+            .ToList();
+
+        var nextPageNode = htmlDoc.DocumentNode
+            .Descendants("div")
+            .Where(e => e.Id == "Pagination")
+            .SelectMany(e => e.Descendants("a").Where(a => a.HasClass("next")))
+            .SingleOrDefault();
+
+        var nextPageRelativeUrl = nextPageNode == null ?
+            string.Empty :
+            nextPageNode.GetAttributeValue("href", string.Empty);
+
+        return (postNodes, nextPageRelativeUrl);
+    }
+}
