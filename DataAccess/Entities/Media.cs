83b7cbaf7f4dd33f6f2f83979932d4ef133a664a
kostyabek <kostyabek@gmail.com>
2022-10-07 10:21:17 +0300

    Added database entities

---
+++ DataAccess/Entities/Media.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Entities.Base;
+
+namespace DataAccess.Entities;
+
+public class Media : BaseIdEntity
+{
+    public string Url { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public Guid? GroupKey { get; set; }
+}
