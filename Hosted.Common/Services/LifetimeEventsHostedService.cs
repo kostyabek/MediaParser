22ec0a3e1c3bc5684ac12fc5fc642fe83cdacbac
kostyabek <kostyabek@gmail.com>
2022-10-07 10:41:18 +0300

    Added lifetime events hosted service

---
+++ Hosted.Common/Services/LifetimeEventsHostedService.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Hosting;
+using NLog;
+
+namespace Hosted.Common.Services;
+
+/// <summary>
+/// Responsible for execution of lifetime-related application events.
+/// </summary>
+public class LifetimeEventsHostedService : IHostedService
+{
+    private readonly IHostApplicationLifetime _appLifetime;
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+    /// <summary>
+    /// Instantiates <see cref="LifetimeEventsHostedService"/>.
+    /// </summary>
+    /// <param name="appLifetime">Host application lifetime.</param>
+    public LifetimeEventsHostedService(IHostApplicationLifetime appLifetime)
+    {
+        _appLifetime = appLifetime;
+    }
+
+    /// <inheritdoc/>
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _appLifetime.ApplicationStopping.Register(OnStopping);
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private void OnStopping()
+    {
+        _logger.Info("Application is stopping.");
+        _logger.Info("Disposing loggers.");
+        LogManager.Shutdown();
+    }
+}
