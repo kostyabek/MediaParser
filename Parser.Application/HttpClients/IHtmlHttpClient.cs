cb296d1bb3d29a0306626ce0c7549cc88c6ccfe2
kostyabek <kostyabek@gmail.com>
2022-10-07 10:36:28 +0300

    Added HtmlHttpClient

---
+++ Parser.Application/HttpClients/IHtmlHttpClient.cs
@@ -0,0 +1,14 @@
+﻿namespace Parser.Application.HttpClients;
+
+/// <summary>
+/// Media HTTP-client.
+/// </summary>
+public interface IHtmlHttpClient
+{
+    /// <summary>
+    /// Retrieves HTML of a page by its URL.
+    /// </summary>
+    /// <param name="url">URL of a target page.</param>
+    /// <returns>Raw HTML.</returns>
+    Task<string?> GetHtml(string url);
+}
---
+++ Parser.Infrastructure/HttpClients/HtmlHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using Parser.Application.HttpClients;
+using RestSharp;
+
+namespace Parser.Infrastructure.HttpClients;
+
+/// <inheritdoc cref="IHtmlHttpClient"/>
+public class HtmlHttpClient : IHtmlHttpClient
+{
+    private readonly RestClient _httpClient;
+
+    /// <summary>
+    /// Instantiates <see cref="HtmlHttpClient"/>.
+    /// </summary>
+    /// <param name="httpClient">Base HTTP-client.</param>
+    public HtmlHttpClient(RestClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    /// <inheritdoc/>
+    public async Task<string?> GetHtml(string url)
+    {
+        var request = new RestRequest(new Uri(url));
+        var response = await _httpClient.ExecuteAsync(request);
+        return response.Content;
+    }
+}
