9f600da9b5a99b42f982cc993b535527a6f9641a
kostyabek <kostyabek@gmail.com>
2022-10-07 10:38:40 +0300

    Added missing consts static class

---
+++ Parser.Domain/AppConsts.cs
@@ -0,0 +1,12 @@
+﻿namespace Parser.Domain;
+
+public static class AppConsts
+{
+    public static class BaseSiteUrls
+    {
+        public static class JoyReactor
+        {
+            public const string BaseDomainName = "https://joyreactor.cc";
+        }
+    }
+}
