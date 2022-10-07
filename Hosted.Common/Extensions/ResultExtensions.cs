1832af926c822a4a3d36edad43fc5127453b1402
kostyabek <kostyabek@gmail.com>
2022-10-07 10:41:02 +0300

    Added missing common extensions

---
+++ Hosted.Common/Extensions/ResultExtensions.cs
@@ -0,0 +1,79 @@
+﻿using Domain.Common.Errors;
+using FluentResults;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hosted.Common.Extensions;
+
+/// <summary>
+/// Contains extensions methods for <see cref="Result"/>.
+/// </summary>
+public static class ResultExtensions
+{
+    /// <summary>
+    /// Converts <see cref="Result"/> to <see cref="IActionResult"/>.
+    /// </summary>
+    /// <param name="result"><see cref="Result"/> object to convert.</param>
+    /// <returns>Correspondent <see cref="IActionResult"/>.</returns>
+    public static IActionResult ToActionResult(this Result result)
+    {
+        return TryGetErrorActionResult(result, out var actionResult) ?
+            actionResult :
+            new OkObjectResult(new
+            {
+                messages = result.Reasons ?? new List<IReason>()
+            });
+    }
+
+    /// <summary>
+    /// Converts <see cref="Result{TValue}"/> to <see cref="IActionResult"/>.
+    /// </summary>
+    /// <param name="result"><see cref="Result{TValue}"/> object to convert.</param>
+    /// <returns>Correspondent <see cref="IActionResult"/>.</returns>
+    public static IActionResult ToActionResult<T>(this Result<T> result)
+    {
+        var nonGenericResult = result.ToResult();
+
+        return TryGetErrorActionResult(nonGenericResult, out var actionResult) ?
+            actionResult :
+            new OkObjectResult(new
+            {
+                value = result.ValueOrDefault,
+                messages = result.Reasons ?? new List<IReason>()
+            });
+    }
+
+    private static bool TryGetErrorActionResult(ResultBase result, out IActionResult actionResult)
+    {
+        actionResult = default;
+
+        if (result.IsSuccess)
+        {
+            return false;
+        }
+
+        var reasons = result.Reasons;
+
+        if (result.HasError<NotFoundError>())
+        {
+            actionResult = new NotFoundObjectResult(reasons);
+            return true;
+        }
+
+        if (result.HasError<InvalidOperationError>())
+        {
+            actionResult = new BadRequestObjectResult(reasons);
+            return true;
+        }
+
+        if (result.HasError<UnauthorizedError>())
+        {
+            actionResult = new UnauthorizedObjectResult(reasons);
+            return true;
+        }
+
+        actionResult = new ObjectResult(reasons) { StatusCode = StatusCodes.Status500InternalServerError };
+
+        return true;
+    }
+}
---
+++ Hosted.Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,116 @@
+﻿using System.Net.Mail;
+using System.Text;
+using Application.Common.Repositories;
+using Domain.Common.Options;
+using Hosted.Common.Services;
+using Infrastructure.Common.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NLog;
+using NLog.Config;
+using NLog.Extensions.Logging;
+using NLog.Layouts;
+using NLog.Targets;
+
+namespace Hosted.Common.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds NLog configuration.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <param name="configuration">Configuration.</param>
+    /// <returns>Service collection.</returns>
+    public static IServiceCollection AddNLogConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        var nLogMailSection = configuration.GetSection("NLog:Mail");
+        var nLogConsoleSection = configuration.GetSection("NLog:Console");
+
+        var nLogConfig = new LoggingConfiguration();
+        var logConsole = new ColoredConsoleTarget("logConsole")
+        {
+            Layout = nLogConsoleSection["Layout"]
+        };
+        var logMail = new MailTarget("logMail")
+        {
+            From = nLogMailSection["From"],
+            To = nLogMailSection["To"],
+            Priority = nLogMailSection["Priority"],
+            AddNewLines = true,
+            Body = new JsonLayout
+            {
+                Attributes =
+                {
+                    new JsonAttribute("date", Layout.FromString("${date}")),
+                    new JsonAttribute("level", Layout.FromString("${level:uppercase=true}")),
+                    new JsonAttribute("message", Layout.FromString("${message}")),
+                    new JsonAttribute("exception", Layout.FromString("${exception:format=ToString}")),
+                    new JsonAttribute("exceptionData", Layout.FromString("${exceptionData}")),
+                },
+                MaxRecursionLimit = 3,
+                IncludeEventProperties = true,
+
+            },
+            EnableSsl = true,
+            DeliveryMethod = SmtpDeliveryMethod.Network,
+            Encoding = Encoding.UTF8,
+            SmtpAuthentication = SmtpAuthenticationMode.Basic,
+            Subject = nLogMailSection["Subject"],
+            SmtpPort = Convert.ToInt32(nLogMailSection["SmtpPort"]),
+            SmtpServer = nLogMailSection["SmtpServer"],
+            SmtpUserName = nLogMailSection["SmtpUsername"],
+            SmtpPassword = nLogMailSection["SmtpPassword"]
+        };
+
+        nLogConfig.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logConsole);
+        nLogConfig.AddRule(NLog.LogLevel.Error, NLog.LogLevel.Fatal, logMail);
+
+        LogManager.Configuration = nLogConfig;
+
+        services.AddLogging(e =>
+        {
+            e.ClearProviders();
+            e.AddNLog(configuration);
+        });
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds custom hosted services.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <returns>Service collection.</returns>
+    public static IServiceCollection AddCustomHostedServices(this IServiceCollection services)
+    {
+        return services.AddHostedService<LifetimeEventsHostedService>();
+    }
+
+    /// <summary>
+    /// Adds configuration for custom <see cref="IOptions{TOptions}"/>.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <param name="configuration">Configuration.</param>
+    /// <returns>Service collection.</returns>
+    public static IServiceCollection AddCustomOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<DatabaseOptions>(configuration.GetSection(nameof(DatabaseOptions)));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers <see cref="IMediaRepository"/> with <see cref="MediaRepository"/> implementation in DI-container.
+    /// </summary>
+    /// <param name="services">Service collection.</param>
+    /// <returns>Service collection.</returns>
+    public static IServiceCollection AddMediaRepository(this IServiceCollection services)
+    {
+        services.AddScoped<IMediaRepository, MediaRepository>();
+
+        return services;
+    }
+}
