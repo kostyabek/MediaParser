647b50ed97de646c8451c61562c895a34f95e6e9
kostyabek <kostyabek@gmail.com>
2022-10-07 10:42:22 +0300

    Added worker for posting

---
+++ Sender.Presentation/Program.cs
@@ -0,0 +1,20 @@
+﻿using Hosted.Common.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(o =>
+    {
+        o.AddUserSecrets(typeof(Program).Assembly);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
+
+        services
+            .AddCustomOptions(configuration)
+            .AddMediaRepository();
+    })
+    .Build();
+
+await host.RunAsync();
